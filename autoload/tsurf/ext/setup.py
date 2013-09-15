bae122ad701819a3a9dad00ce927916c5aa246f7
thibaud bernard <thibaud.bernard69@gmail.com>
2013-09-15 20:35:48 +0200

    option need for compilation on Ubuntu

--- autoload/tsurf/ext/setup.py
+++ autoload/tsurf/ext/setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup, Extension
 
 setup(
     ext_modules = [
-        Extension('search', sources = ['searchmodule.c'])
+        Extension('search', sources = ['searchmodule.c'],extra_compile_args=['-std=c99'])
     ]
 )
